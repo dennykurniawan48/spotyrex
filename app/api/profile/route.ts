aaa058c1eecaab24515bcaa41f503e0b9fb48f46
Denny Kurniawan <cipher.denny@gmail.com>
2023-11-07 22:45:33 +0700

    add profile api route

---
+++ app/api/profile/route.ts
@@ -0,0 +1,11 @@
+import { getToken } from "next-auth/jwt"
+import { NextRequest } from "next/server"
+const jwt = require("jwt")
+
+export async function GET(req: Request){
+    const token = await getToken({ req: req as NextRequest, secret: process.env.NEXTAUTH_SECRET, raw: true })
+    console.log(token);
+    const user = jwt.verify(token, process.env.NEXTAUTH_SECRET)
+    console.log(user)
+    return Response.json({data: user})
+}
