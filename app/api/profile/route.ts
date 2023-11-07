bfb6ce2a4e5941f419651b0f71bbdacc5a548ad8
Denny Kurniawan <cipher.denny@gmail.com>
2023-11-07 22:48:27 +0700

    fix wrong import jwt module

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,6 +1,6 @@
 import { getToken } from "next-auth/jwt"
 import { NextRequest } from "next/server"
-const jwt = require("jwt")
+const jwt = require("jsonwebtoken")
 
 export async function GET(req: Request){
     const token = await getToken({ req: req as NextRequest, secret: process.env.NEXTAUTH_SECRET, raw: true })
